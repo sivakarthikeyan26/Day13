82de7e18e64c60a89cb1e0ab972eeed7b56d9058
Your Name <you@example.com>
2022-07-11 20:29:50 +0530

    program to implement generics is implemented

--- src/day13/Generics_func.java
+++ src/day13/Generics_func.java
@@ -4,12 +4,21 @@ public class Generics_func<T extends Comparable<T>> {
 	private T a;
 	private T b;
 	private T c;
-	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * constructor to accept the arguments
+	 */
 	public Generics_func(T a, T b, T c) {
 		this.a=a;
 		this.b=b;
 		this.c=c;
 	}
+	/**
+	 * method to comapre the values
+	 */
 	public void compare() {
 		if(a.compareTo(b)>0) {
 			if(a.compareTo(c)>0) {
@@ -21,6 +30,9 @@ public class Generics_func<T extends Comparable<T>> {
 		}
 		else if(b.compareTo(c)>0) {
 			System.out.println(b+" is greater");
+		}
+		else {
+			System.out.println(c+" is greater");
 		}
 	}
 	
--- src/day13/Generics_main.java
+++ src/day13/Generics_main.java
@@ -1,10 +1,11 @@
 package day13;
 
-import java.util.Scanner;
 
 public class Generics_main {
-	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
+		/*
+		 * creating object to implement different data types.
+		 */
 		Generics_func<Integer> g =  new Generics_func<Integer>(10,20,30);
 		Generics_func<String> g1 =  new Generics_func<String>("Siva","siva2","parrot");
 		g.compare();
